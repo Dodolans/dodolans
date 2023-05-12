3c06f2d58a3afc59813228482a19c37eaa5801bd
Dodolans <133308061+Dodolans@users.noreply.github.com>
2023-05-12 11:12:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
 console.log('readt----------.');
 
 $(document).ready(function() {
-    $.getJSON( "https://gist.githubusercontent.com/Rahmadjaya/4f766d046ee40fe80761116a0fc0955e/raw/a326ca23332609d7744b80efdde075c5bbd7a594/products.json", function( json ) {
-        console.log(json);
+    const urlApi = "https://gist.githubusercontent.com/Rahmadjaya/4f766d046ee40fe80761116a0fc0955e/raw/a326ca23332609d7744b80efdde075c5bbd7a594/products.json";
+
+    function appendItemMovie(data) {
+      for(var i = 0; i <= data.length;i++){
+        if(data[i] != undefined){
+          var dataHtml = `
+                              <ion-col size="6">
+                                <ion-card class="ion-no-margin" style="height: 100%;">
+                                  <img alt="Silhouette of mountains" src="`+data[i].image+`" />
+                                  <ion-card-header>
+                                    <ion-card-title>`+data[i].product_name+`</ion-card-title>
+                                    <ion-card-subtitle>`+data[i].price+`</ion-card-subtitle>
+                                  </ion-card-header>
+                                </ion-card>
+                              </ion-col>
+                          `;
+          $('#PlaylistMovie').append(dataHtml)
+        }
+      }
+    }
+
+    $.get(urlApi, function(data, status){
+        console.log(JSON.parse(data));
+      appendItemMovie(JSON.parse(data));
+      searchProduct()
     });
-    
+
+    function searchProduct() {
+      const searchbar = document.querySelector('ion-searchbar');
+      const items = Array.from(document.getElementById('PlaylistMovie').children);
+      console.log('items', items)
+      searchbar.addEventListener('ionInput', handleInput);
+      function handleInput(event) {
+        console.log(event)
+        const query = event.target.value.toLowerCase();
+        requestAnimationFrame(() => {
+          items.forEach(item => {
+            const shouldShow = item.textContent.toLowerCase().indexOf(query) > -1;
+            item.style.display = shouldShow ? 'block' : 'none';
+          });
+        });
+      }
+    }
+
     
  
        
